169c3a997c6e9effaf7b448210c2f6e3c71d2f8a
Eloisa-R <rios.rico.eloisa@gmail.com>
2018-03-28 10:06:05 +0200

    Testing API call

--- js/login.js
+++ js/login.js
@@ -7,15 +7,31 @@
 var apiApp = new Vue({
     el: '#apiApp',
     data: {
-        access_token: "",},
+        access_token: "",
+        contries_location: {},
+        location_response: ""
+    },
     
     methods: {
         getAccessToken: function() {
-            return window.location.href.split("#access_token")[1]
+            console.log("yay")
+            return window.location.href.split("#access_token=")[1]
+        },
+        
+        getLocationData: function() {
+            $.getJSON("https://api.myjson.com/bins/l9twf", function(data) {
+                apiApp.countries_location = data,
+                $.getJSON("https://api.instagram.com/v1/locations/search?lat=40.463667&lng=-3.74922&access_token=" + apiApp.access_token, function(data) {
+                    apiApp.location_response = data
+                })
+            })
         }
     },
     
     created: function() {
+        if (window.location.href.includes("#access_token")) {
+            this.access_token = this.getAccessToken()
+        }
         
         
     },
